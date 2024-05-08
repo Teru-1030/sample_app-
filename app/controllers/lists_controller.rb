01d2a2505801cce915230288d3f8aa9d96c94163
Teru-1030 <166922869+Teru-1030@users.noreply.github.com>
2024-05-08 12:01:03 +0000

    Initial Commit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,17 @@
 class ListsController < ApplicationController
   def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
   end
-
+  
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
+  end  
   def index
   end
 
@@ -10,4 +20,9 @@ class ListsController < ApplicationController
 
   def edit
   end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    parms.require(:list).permit(:title, :body)
 end
